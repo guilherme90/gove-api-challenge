007e16d12ede518e04184fdee4a1970c52771c6f
Guilherme Nogueira <guilhermenogueira90@gmail.com>
2024-01-22 15:30:44 -0300

    refactor: ajustando schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new NotifyContactsJob(new NotifyContactUseCase))->everyTenSeconds()->runInBackground();
+        $schedule->job(new NotifyContactsJob(new NotifyContactUseCase))->everyFiveMinutes();
     }
 
     /**
